cd3408b865928d5367f7a465837cd176517dd35d
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2024-02-26 22:59:02 +0100

    amateur  middleware

---
+++ backend/cmd/server/middleware.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"server"
+	"server/types"
+
+	"github.com/gin-gonic/gin"
+)
+
+// func authMiddleware(authTokenService types.AuthTokenService) gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		ctx := c.Request.Context()
+
+// 		log.Printf("token: %v", c.Request)
+// 		token, err := authTokenService.ParseTokenFromRequest(ctx, c.Request)
+// 		log.Printf("token: %v", token)
+// 		if err != nil {
+// 			c.Next()
+// 			return
+// 		}
+
+// 		ctx = server.PutUserIDIntoContext(ctx, token.Sub)
+// 		c.Request = c.Request.WithContext(ctx)
+
+//			c.Next()
+//		}
+//	}
+func authMiddleware(authTokenService types.AuthTokenService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx := c.Request.Context()
+
+		token, err := authTokenService.ParseTokenFromRequest(ctx, c.Request)
+		if err != nil {
+			c.Next()
+			return
+		}
+
+		// Log the parsed token
+		ctx = server.PutUserIDIntoContext(ctx, token.Sub)
+		c.Request = c.Request.WithContext(ctx)
+		c.Next()
+	}
+}
