bd385916b03611c2c0e02bb89fbc858c15fbcff2
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2024-02-26 22:56:30 +0100

    types

---
+++ backend/types/user.go
@@ -0,0 +1,20 @@
+package types
+
+import (
+	"context"
+	"server/pkg/model"
+)
+
+type UserService interface {
+	CreateUser(ctx context.Context, user model.User) (*model.User, error)
+	GetUserByID(ctx context.Context, id string) (*model.User, error)
+	GetUserByEmail(ctx context.Context, email string) (*model.User, error)
+	GetAllUsers(ctx context.Context) ([]model.User, error)
+}
+
+type UserRepo interface {
+	Create(ctx context.Context, user model.User) (*model.User, error)
+	GetByEmail(ctx context.Context, email string) (*model.User, error)
+	GetByID(ctx context.Context, id string) (*model.User, error)
+	GetAll(ctx context.Context) ([]*model.User, error)
+}
