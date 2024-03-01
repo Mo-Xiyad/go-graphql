f4afbf6887df9b0a6e22506cd257b1c003330b3d
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2024-03-01 23:52:28 +0100

    creal middleware

--- backend/cmd/server/middleware.go
+++ backend/cmd/server/middleware.go
@@ -16,7 +16,6 @@ import (
 func authMiddleware(authTokenService services.IAuthTokenService, allowedList map[string]bool) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ctx := c.Request.Context()
-		ctx = server.SetIsLoggedIn(ctx, false)
 
 		operationName := getOperationName(c)
 		if allowedList[operationName] {
@@ -33,7 +32,6 @@ func authMiddleware(authTokenService services.IAuthTokenService, allowedList map
 			return
 		}
 
-		ctx = server.SetIsLoggedIn(ctx, true)
 		ctx = server.PutUserIDIntoContext(ctx, token.UserID)
 
 		c.Request = c.Request.WithContext(ctx)
